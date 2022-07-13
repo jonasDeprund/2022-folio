fd9502da15cefb086df47a33c41b97983ab06b4f
Jonas Deprund <jonasdeprund@gmail.com>
2022-07-13 08:23:19 +0200

    project done

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import imageNexity from '../images/nexity.gif';
 import imageBonduelle from '../images/bonduelle.gif';
 import imageDanone from '../images/danone.gif';
 import imageElo from '../images/elo.gif';
 
 function Container() {
+  const [isShown1, setIsShown1] = useState(false);
+  const [isShown2, setIsShown2] = useState(false);
+  const [isShown3, setIsShown3] = useState(false);
+  const [isShown4, setIsShown4] = useState(false);
+
   return (
     <main class="wrapper-main">
       {/* text */}
@@ -13,11 +19,57 @@ function Container() {
           <h1 class="text-h1 cell-start-0 cell-end-12 cell-start-0-m cell-end-12-m">
             Hello. I am an interactive designer based in Paris.
             <br />I worked for
-            <span class="text-h1 button">Nexity</span>
-            <span class="text-h1 button">Bonduelle</span>
-            <span class="text-h1 button">Danone</span>
-            <span class="text-h1 button">Elo Group</span>
-            <span class="text-h1 button">+</span>
+            <a
+              href="https://www.behance.net/gallery/122497029/Nexity-Corporate-website-%28Pitch%29"
+              target="_blank"
+            >
+              <span
+                onMouseEnter={() => setIsShown1(true)}
+                onMouseLeave={() => setIsShown1(false)}
+                class="text-h1 button"
+              >
+                Nexity
+              </span>
+            </a>
+            <a
+              href="https://www.behance.net/gallery/132516291/Bonduelle-Corporate-Website"
+              target="_blank"
+            >
+              <span
+                onMouseEnter={() => setIsShown2(true)}
+                onMouseLeave={() => setIsShown2(false)}
+                class="text-h1 button"
+              >
+                Bonduelle
+              </span>
+            </a>
+            <a
+              href="https://www.behance.net/gallery/124320911/Danone-Annual-report-2020"
+              target="_blank"
+            >
+              <span
+                onMouseEnter={() => setIsShown3(true)}
+                onMouseLeave={() => setIsShown3(false)}
+                class="text-h1 button"
+              >
+                Danone
+              </span>
+            </a>
+            <a
+              href="https://www.behance.net/gallery/131689655/Elo-Group-Corporate-Website"
+              target="_blank"
+            >
+              <span
+                onMouseEnter={() => setIsShown4(true)}
+                onMouseLeave={() => setIsShown4(false)}
+                class="text-h1 button"
+              >
+                Elo Group
+              </span>
+            </a>
+            <a href="https://www.behance.net/jonasdeprund" target="_blank">
+              <span class="text-h1 button">+</span>
+            </a>
             <br />
             <br />
             Feel free to contact me
@@ -28,38 +80,46 @@ function Container() {
       <div className="container-images">
         <div className="bloc-image image-1">
           <div className="row">
-            <img
-              className="image-gif cell-start-6 cell-end-10"
-              src={imageNexity}
-              alt=""
-            />
+            {isShown1 && (
+              <img
+                className="image-gif cell-start-5 cell-end-10"
+                src={imageNexity}
+                alt=""
+              />
+            )}
           </div>
         </div>
         <div className="bloc-image image-2">
           <div className="row">
-            <img
-              className="image-gif cell-start-0 cell-end-4"
-              src={imageBonduelle}
-              alt=""
-            />
+            {isShown2 && (
+              <img
+                className="image-gif cell-start-0 cell-end-5"
+                src={imageBonduelle}
+                alt=""
+              />
+            )}
           </div>
         </div>
         <div className="bloc-image image-3">
           <div className="row">
-            <img
-              className="image-gif cell-start-3 cell-end-7"
-              src={imageDanone}
-              alt=""
-            />
+            {isShown3 && (
+              <img
+                className="image-gif cell-start-3 cell-end-8"
+                src={imageDanone}
+                alt=""
+              />
+            )}
           </div>
         </div>
         <div className="bloc-image image-4">
           <div className="row">
-            <img
-              className="image-gif cell-start-8 cell-end-12"
-              src={imageElo}
-              alt=""
-            />
+            {isShown4 && (
+              <img
+                className="image-gif cell-start-7 cell-end-12"
+                src={imageElo}
+                alt=""
+              />
+            )}
           </div>
         </div>
       </div>
