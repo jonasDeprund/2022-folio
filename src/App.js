3292996a8a677fc0545c25715b80443b391bf89c
Jonas Deprund <jonas@MacBook-Pro-de-Jonas.local>
2022-09-28 18:13:11 +0200

    setup added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import Background from './components/Background';
 import Container from './components/Container';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './scss/_main.scss'; // assuming a styles directory under src/
+import './js/shape.js';
 
 function App() {
   return (
     <div className="main">
       <Header />
       <Container />
+      <Background />
       <Footer />
     </div>
   );
