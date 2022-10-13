03a144f196c56acfe8aaa367e5c47a999b623a23
Jonas Deprund <jonasdeprund@gmail.com>
2022-10-13 18:08:57 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Container from './components/Container';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './scss/_main.scss'; // assuming a styles directory under src/
-import './js/shape.js';
 
 function App() {
   return (
