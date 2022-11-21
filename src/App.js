c95cef582eb5baa834e847b950fce8f262e86539
Jonas Deprund <jonasdeprund@gmail.com>
2022-11-21 18:00:40 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Background, { Sketch } from './components/Background';
-import Container from './components/Container';
+// import Container from './components/Container';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import './scss/_main.scss'; // assuming a styles directory under src/
@@ -9,7 +9,7 @@ function App() {
     <div className="main">
       <Header />
       <Background />
-      <Container />
+      {/* <Container /> */}
       <Footer />
     </div>
   );
--- src/components/Background.js
+++ src/components/Background.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Import three
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Import layers
-import Fragment from '../shader/fragment.glsl';
-import Vertex from '../shader/vertex.glsl';
-// import GUI from 'lil-gui';
-import gsap from 'gsap';
+import fragment from '../shader/fragment.glsl';
+import vertex from '../shader/vertex.glsl';
+// import gui from 'lil-gui';
+// import gsap from 'gsap';
 
-export class Sketch extends React.PureComponent {
+class Sketch extends Component {
   constructor(options) {
+    super(options);
     this.scene = new THREE.Scene();
 
     this.container = options.dom;
@@ -45,14 +46,14 @@ export class Sketch extends React.PureComponent {
     this.setupResize();
   }
 
-  settings() {
-    let that = this;
-    this.settings = {
-      progress: 0,
-    };
-    this.gui = new gui();
-    this.gui.add(this.settings, 'progress', 0, 1, 0.01);
-  }
+  // settings() {
+  //   let that = this;
+  //   this.settings = {
+  //     progress: 0,
+  //   };
+  //   this.gui = new gui();
+  //   this.gui.add(this.settings, 'progress', 0, 1, 0.01);
+  // }
 
   setupResize() {
     window.addEventListener('resize', this.resize.bind(this));
@@ -84,7 +85,7 @@ export class Sketch extends React.PureComponent {
   }
 
   addObjects() {
-    let that = this;
+    // let that = this;
     this.material = new THREE.ShaderMaterial({
       extensions: {
         derivatives: '#extension GL_OES_standard_derivatives : enable',
@@ -104,136 +105,41 @@ export class Sketch extends React.PureComponent {
     this.scene.add(this.plane);
   }
 
-  addLights(){
-      const light1 = new THREE.AmbientLight(0xffffff, 0.5);
-      this.scene.add(light1);
-    
-      const light2 = new THREE.DirectionalLight(0xffffff, 0.5);
-      light2.position.set(0.5, 0, 0.866);
-      this.scene.add(light2);
-    }
+  addLights() {
+    const light1 = new THREE.AmbientLight(0xffffff, 0.5);
+    this.scene.add(light1);
 
-    stop(){
-      this.isPlaying = false;
-    }
-    
-    play(){
-      if(!this.isPlaying){
-        this.isPlaying = true;
-        this.render()
-      }
-    }
+    const light2 = new THREE.DirectionalLight(0xffffff, 0.5);
+    light2.position.set(0.5, 0, 0.866);
+    this.scene.add(light2);
+  }
 
-render(){
-  if(!this.isPlaying) return;
-  this.time += 0.05;
-  this.material.uniforms.time.value = this.time;
-  requestAnimationFrame(this.render.bind(this));
-  this.renderer.render(this.scene, this.camera);
+  stop() {
+    this.isPlaying = false;
+  }
+
+  play() {
+    if (!this.isPlaying) {
+      this.isPlaying = true;
+      this.render();
+    }
+  }
+
+  render() {
+    if (!this.isPlaying) return;
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    requestAnimationFrame(this.render.bind(this));
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
-new Sketch(){
-  dom: document.getElementsByClassName("shape")    
-}
+new Sketch({
+  dom: document.getElementsByClassName('shape'),
+});
 
 function Background() {
   return <div class="shape"></div>;
 }
 
 export default Background;
-
-////////////////////////////////////////////////////////////////
-
-// // viwport
-// var SCREEN_WIDTH = 1000;
-// var SCREEN_HEIGHT = 600;
-
-// //renderer
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-// document.body.appendChild(renderer.domElement);
-
-// //scene
-// var scene = new THREE.Scene();
-
-// //camera
-// var camera = new THREE.PerspectiveCamera(
-//   45,
-//   SCREEN_WIDTH / SCREEN_HEIGHT,
-//   1,
-//   1000
-// );
-// var vFOV = camera.fov * (Math.PI / 180);
-// camera.position.z = SCREEN_HEIGHT / (2 * Math.tan(vFOV / 2));
-
-// //plane
-// var geometry = new THREE.PlaneGeometry(300, 300, 300, 300);
-// var material = new THREE.MeshBasicMaterial({
-//   color: 0xffff00,
-//   side: THREE.DoubleSide,
-//   wireframe: true,
-// });
-// var plane = new THREE.Mesh(geometry, material);
-// scene.add(plane);
-
-// function render() {
-//   requestAnimationFrame(render);
-//   plane.rotation.y += 0.1;
-//   renderer.render(scene, camera);
-// }
-
-// render();
-
-// // new code
-// class Sketch {
-// addObjects() {
-//   extensions: {
-//   },
-//   side: THREE.DoubleSide,
-//   uniforms: {
-//     time: {value: 0},
-//     resolution: {value: new THREE.Vector4()},
-//   },
-//   wireframe: true,
-//   vertexShader: Vertex,
-//   fragmentShader: Fragment,
-// });
-
-// this.geometry = new THREE.PlaneGeometry(1,1,300,300);
-
-// this.plane = new THREE.Mesh(this.geometry, this.material);
-// this.scene.add(plane);
-// }
-
-// addLights(){
-//   const light1 = new THREE.AmbientLight(0xffffff, 0.5);
-//   this.scene.add(light1);
-
-//   const light2 = new THREE.DirectionalLight(0xffffff, 0.5);
-//   light2.position.set(0.5, 0, 0.866);
-//   this.scene.add(light2);
-// }
-
-// stop(){
-//   this.isPlaying = false;
-// }
-
-// play(){
-//   if(!this.isPlaying){
-//     this.isPlaying = true;
-//     this.render()
-//   }
-
-// render(){
-//   if(!this.isPlaying) return;
-//   this.time += 0.05;
-//   this.material.uniforms.time.value = this.time;
-//   requestAnimationFrame(this.render.bind(this));
-//   this.renderer.render(this.scene, this.camera);
-// }
-// }
-
-// new Sketch({
-//   dom: document.getElementsByClassName("shape")
-// });
