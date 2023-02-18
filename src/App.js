29de4aab84a72e50c59f844b9d32ffaec8057052
Jonas Deprund <jonasdeprund@gmail.com>
2023-02-18 18:17:07 +0100

    images added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Background from './components/Background';
 
 // Component
 import Footer from './components/Footer';
+import Header from './components/Header';
 import Nav from './components/Nav';
 import Projects from './components/Projects';
 
@@ -15,6 +17,8 @@ function App() {
         <Nav />
       </nav>
       <main>
+        <Header />
+        <Background />
         <Projects />
       </main>
       <footer>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,42 +1,49 @@
 import React from 'react';
 
+import imageNexity from '../images/thumb-nexity.png';
+import imageBonduelle from '../images/thumb-bonduelle.png';
+import imageDanone from '../images/thumb-danone.png';
+import imageEloGroup from '../images/thumb-elogroup.png';
+import imageAhp from '../images/thumb-ahp.png';
+import imageSoBrico from '../images/thumb-sobrico.png';
+
 const Projects = () => {
   return (
     <div className="row">
       <div className="cell-start-0 cell-end-6">
-        <img src="./images/thumb-bonduelle.png" alt="Project 1" />
+        <img src={imageNexity} alt="Nexity" />
         <div className="">
-          <h3 className="text-h2">Project 1</h3>
+          <h3 className="text-h2">Nexity</h3>
         </div>
       </div>
       <div className="cell-start-6 cell-end-12">
-        <img src="./images/thumb-bonduelle.png" alt="Project 1" />
+        <img src={imageBonduelle} alt="Bonduelle" />
         <div className="">
-          <h3 className="text-h2">Project 1</h3>
+          <h3 className="text-h2">Bonduelle</h3>
         </div>
       </div>
       <div className="cell-start-0 cell-end-6">
-        <img src="./images/thumb-bonduelle.png" alt="Project 1" />
+        <img src={imageDanone} alt="Danone" />
         <div className="">
-          <h3 className="text-h2">Project 1</h3>
+          <h3 className="text-h2">Danone</h3>
         </div>
       </div>
       <div className="cell-start-6 cell-end-12">
-        <img src="./images/thumb-bonduelle.png" alt="Project 1" />
+        <img src={imageEloGroup} alt="Elo Group" />
         <div className="">
-          <h3 className="text-h2">Project 1</h3>
+          <h3 className="text-h2">Elo Group</h3>
         </div>
       </div>
       <div className="cell-start-0 cell-end-6">
-        <img src="./images/thumb-bonduelle.png" alt="Project 1" />
+        <img src={imageAhp} alt="AHP" />
         <div className="">
-          <h3 className="text-h2">Project 1</h3>
+          <h3 className="text-h2">AHP</h3>
         </div>
       </div>
       <div className="cell-start-6 cell-end-12">
-        <img src="./images/thumb-bonduelle.png" alt="Project 1" />
+        <img src={imageSoBrico} alt="So Brico" />
         <div className="">
-          <h3 className="text-h2">Project 1</h3>
+          <h3 className="text-h2">So Brico</h3>
         </div>
       </div>
     </div>
