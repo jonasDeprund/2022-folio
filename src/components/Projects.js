f9a914952af68c3c244150bb5832d9366524f297
Jonas Deprund <jonasdeprund@gmail.com>
2023-02-27 18:37:23 +0100

    description added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import imageDanone from '../images/thumb-danone.png';
 import imageEloGroup from '../images/thumb-elogroup.png';
 import imageAhp from '../images/thumb-ahp.png';
 import imageSoBrico from '../images/thumb-sobrico.png';
+import iconArrow from '../svg/arrow.svg';
 
 const Projects = () => {
   return (
@@ -13,7 +14,10 @@ const Projects = () => {
       <div className="cell-start-0 cell-end-6">
         <div className="project__content">
           <img className="project__image" src={imageNexity} alt="Nexity" />
-          <h3 className="text-h2">Nexity</h3>
+          <div className="project__description">
+            <h3 className="text-h2">Nexity</h3>
+            <img className="project__icon" src={iconArrow}></img>
+          </div>
         </div>
       </div>
       <div className="cell-start-6 cell-end-12">
@@ -23,31 +27,46 @@ const Projects = () => {
             src={imageBonduelle}
             alt="Bonduelle"
           />
-          <h3 className="text-h2">Bonduelle</h3>
+          <div className="project__description">
+            <h3 className="text-h2">Bonduelle</h3>
+            <img className="project__icon" src={iconArrow}></img>
+          </div>
         </div>
       </div>
       <div className="cell-start-0 cell-end-6">
         <div className="project__content">
           <img className="project__image" src={imageDanone} alt="Danone" />
-          <h3 className="text-h2">Danone</h3>
+          <div className="project__description">
+            <h3 className="text-h2">Danone</h3>
+            <img className="project__icon" src={iconArrow}></img>
+          </div>
         </div>
       </div>
       <div className="cell-start-6 cell-end-12">
         <div className="project__content">
           <img className="project__image" src={imageEloGroup} alt="Elo Group" />
-          <h3 className="text-h2">Elo Group</h3>
+          <div className="project__description">
+            <h3 className="text-h2">Elo Group</h3>
+            <img className="project__icon" src={iconArrow}></img>
+          </div>
         </div>
       </div>
       <div className="cell-start-0 cell-end-6">
         <div className="project__content">
           <img className="project__image" src={imageAhp} alt="AHP" />
-          <h3 className="text-h2">AHP</h3>
+          <div className="project__description">
+            <h3 className="text-h2">AHP</h3>
+            <img className="project__icon" src={iconArrow}></img>
+          </div>
         </div>
       </div>
       <div className="cell-start-6 cell-end-12">
         <div className="project__content">
           <img className="project__image" src={imageSoBrico} alt="So Brico" />
-          <h3 className="text-h2">So Brico</h3>
+          <div className="project__description">
+            <h3 className="text-h2">So Brico</h3>
+            <img className="project__icon" src={iconArrow}></img>
+          </div>
         </div>
       </div>
     </div>
