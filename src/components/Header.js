7355760e7d6c6a542209b2539fae363d8b94097b
Jonas Deprund <jonasdeprund@gmail.com>
2023-03-18 16:21:54 +0100

    added hover zoom

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,11 +10,13 @@ function Header() {
     <div className="header">
       {/* text */}
       <div className="row">
-        <h1 className="text-h1 text-h1-m cell-start-0 cell-end-12 cell-start-0-m cell-end-12-m">
-          Hello,
-          <br></br>I am an interactive designer based in Paris.
-          <br></br>I craft identities and websites.
-        </h1>
+        <div className="header__title cell-start-0 cell-end-12 cell-start-0-m cell-end-12-m">
+          <h1 className="text-h1 text-h1-m ">
+            Hello,
+            <br></br>I am an interactive designer based in Paris.
+            <br></br>I craft identities and websites.
+          </h1>
+        </div>
       </div>
       <Background />
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,9 @@ const Projects = () => {
     <div className="row">
       <div className="cell-start-0 cell-end-6">
         <div className="project__content">
-          <img className="project__image" src={imageNexity} alt="Nexity" />
+          <div className="project__content-image">
+            <img className="project__image" src={imageNexity} alt="Nexity" />
+          </div>
           <div className="project__description">
             <h3 className="text-h2">Nexity</h3>
             <img className="project__icon" src={iconArrow}></img>
@@ -22,11 +24,13 @@ const Projects = () => {
       </div>
       <div className="cell-start-6 cell-end-12">
         <div className="project__content">
-          <img
-            className="project__image"
-            src={imageBonduelle}
-            alt="Bonduelle"
-          />
+          <div className="project__content-image">
+            <img
+              className="project__image"
+              src={imageBonduelle}
+              alt="Bonduelle"
+            />
+          </div>
           <div className="project__description">
             <h3 className="text-h2">Bonduelle</h3>
             <img className="project__icon" src={iconArrow}></img>
@@ -35,7 +39,9 @@ const Projects = () => {
       </div>
       <div className="cell-start-0 cell-end-6">
         <div className="project__content">
-          <img className="project__image" src={imageDanone} alt="Danone" />
+          <div className="project__content-image">
+            <img className="project__image" src={imageDanone} alt="Danone" />
+          </div>
           <div className="project__description">
             <h3 className="text-h2">Danone</h3>
             <img className="project__icon" src={iconArrow}></img>
@@ -44,7 +50,13 @@ const Projects = () => {
       </div>
       <div className="cell-start-6 cell-end-12">
         <div className="project__content">
-          <img className="project__image" src={imageEloGroup} alt="Elo Group" />
+          <div className="project__content-image">
+            <img
+              className="project__image"
+              src={imageEloGroup}
+              alt="Elo Group"
+            />
+          </div>
           <div className="project__description">
             <h3 className="text-h2">Elo Group</h3>
             <img className="project__icon" src={iconArrow}></img>
@@ -53,7 +65,9 @@ const Projects = () => {
       </div>
       <div className="cell-start-0 cell-end-6">
         <div className="project__content">
-          <img className="project__image" src={imageAhp} alt="AHP" />
+          <div className="project__content-image">
+            <img className="project__image" src={imageAhp} alt="AHP" />
+          </div>
           <div className="project__description">
             <h3 className="text-h2">AHP</h3>
             <img className="project__icon" src={iconArrow}></img>
@@ -62,7 +76,9 @@ const Projects = () => {
       </div>
       <div className="cell-start-6 cell-end-12">
         <div className="project__content">
-          <img className="project__image" src={imageSoBrico} alt="So Brico" />
+          <div className="project__content-image">
+            <img className="project__image" src={imageSoBrico} alt="So Brico" />
+          </div>
           <div className="project__description">
             <h3 className="text-h2">So Brico</h3>
             <img className="project__icon" src={iconArrow}></img>
